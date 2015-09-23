962e4c718f30f8d3726188e286bf011768426bee
Kai Millner <kmillner@github.com>
2015-09-23 10:59:07 -0700

    Beginning tic tac toe tests and scripts

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var player1 = 'x'; // turn = 0
+var player2 = 'o'; // turn = 1
+var turn = 0; //starts with player1
+
+var boardCheck; // function to check value in each cell
+var a1; // value within each cell but how to build the board???
+var a2;
+var a3;
+var b1;
+var b2;
+var b3;
+var c1;
+var c2;
+var c3;
+
+var checkWin;
+var xWin = false; // true if X wins
+var oWin = false; // true if O wins
+var winAlert; // function that declares winner and restarts game
+
+var newGame;
+var clearBoard;
+
+var newGame = function() {
+  
+}
+ //if else statement and toggling for player one to put the 'x' and player 2 to put the 'o'?
